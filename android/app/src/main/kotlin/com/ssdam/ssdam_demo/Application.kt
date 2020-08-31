044e87c6a03540f804f75c368f33e4c119a8344a
Acver14 <cmak139@gmail.com>
2020-08-31 16:10:44 +0900

    2020/08/31

---
+++ android/app/src/main/kotlin/com/ssdam/ssdam_demo/Application.kt
@@ -0,0 +1,22 @@
+package com.ssdam.ssdam_demo
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import io.flutter.app.FlutterApplication
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService
+
+
+const val CHANNEL_HIGH = "test"
+class Application : FlutterApplication(), PluginRegistrantCallback {
+    override fun onCreate() {
+        super.onCreate()
+        FlutterFirebaseMessagingService.setPluginRegistrant(this)
+    }
+
+    override fun registerWith(registry: PluginRegistry) {
+        FirebaseCloudMessagingPluginRegistrant.registerWith(registry)
+    }
+}
